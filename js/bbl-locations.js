d917357e8c4dd06a21445b71726bc87084747b6e
Romain Linsolas <linsolas@gmail.com>
2013-02-20 15:46:42 +0100

    Test d'ajout de la carte Google + "fake" data (Toulouse).

--- js/bbl-locations.js
+++ js/bbl-locations.js
@@ -4,15 +4,32 @@ var bblLocationsData = {
             pseudo: "sgcib",
             name: "Société Générale - SGCIB",
             address: "Tours SG, La Défense (92)",
+            gps: "48.891601,2.229495",
             website: "http://www.societegenerale.fr",
             contact: "Romain Linsolas (linsolas [at] gmail.com)",
             picture: "http://www.societegenerale.fr/images/eimm_logo.png",
             interests: [
-                    "Java", "Développement", "Agilité"
+                    "Java", "Développement", "Développement Web", "Agilité"
             ],
             audience: {
                 profiles: "Développeurs, Architectes, Chefs de projets",
                 number: "Au moins 20 personnes"
+            }
+        },
+        {   // Données de test
+            pseudo: "toulouse",
+            name: "TBA",
+            address: "Toulouse",
+            gps: "43.604482,1.443962",
+            website: "",
+            contact: "",
+            picture: "http://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Blason_de_Toulouse.png/100px-Blason_de_Toulouse.png",
+            interests: [
+                    "Développement", "Agilité"
+            ],
+            audience: {
+                profiles: "Développeurs, Architectes, Chefs de projets",
+                number: "10 personnes"
             }
         }
     ]
