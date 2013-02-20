b50fda86059d9c5c075bada18a1585bfe61ae462
Romain Linsolas <linsolas@gmail.com>
2013-02-20 09:51:30 +0100

    Séparation données / présentations pour les lieux.

---
+++ js/bbl-locations.js
@@ -0,0 +1,19 @@
+var bblLocationsData = {
+    locations:[
+        {
+            pseudo: "sgcib",
+            name: "Société Générale - SGCIB",
+            address: "Tours SG, La Défense (92)",
+            website: "http://www.societegenerale.fr",
+            contact: "Romain Linsolas (linsolas [at] gmail.com)",
+            picture: "http://www.societegenerale.fr/images/eimm_logo.png",
+            interests: [
+                    "Java", "Développement", "Agilité"
+            ],
+            audience: {
+                profiles: "Développeurs, Architectes, Chefs de projets",
+                number: "Au moins 20 personnes"
+            }
+        }
+    ]
+};
