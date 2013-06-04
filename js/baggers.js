81ee37aecb115277d705147741d8eece2721dff6
Romain Linsolas <linsolas@gmail.com>
2013-06-04 13:02:56 +0300

    Ajout de Thierry Leriche-Dessirier

--- js/baggers.js
+++ js/baggers.js
@@ -697,6 +697,24 @@ var data = {
                         }
                     ],
                     tags: ["JavaEE", "J2EE", "CDI","Arquillian"]
+                },
+                {
+                    name: "Thierry Leriche-Dessirier",
+                    bio: "Consultant freelance, intervenant sur des projets Web JEE Agile, de l'architecture et/ou du développement",
+                    picture: "http://icauda.com/img/thierry3.jpg",
+                    websites: [
+                        {title: "Mon site", href: "http://icauda.com"}
+                    ],
+                    twitter: "thierryleriche",
+                    contact: "thierryler@gmail.com",
+                    location: "Paris",
+                    sessions: [
+                        {
+                            title: "Présentation de Guava et Lombok",
+                            summary: "Guava est une librairie open source créée et maintenue par les équipes de Google. Inspirée des outils internes du géant, Guava propose de nombreux mécanismes (programmation fonctionnelle) pour manipuler les collections, créer des caches customisés, faciliter la gestion de la concurrence et les IOs.<br/>De son coté, Lombok est également un projet Open Source. Lombok et Lombok-pg simplifient sensiblement l'écriture de code classique (builders, delegates, etc.) et des nombreuses méthodes purement techniques et indispensables (getters, setters, hashCode, equals, etc.) qui polluent nos objets.<br/>Durant cette présentation, après un rapide constat de la verbosité de nos beans, nous feront un tour d'horizon de Lombok et Guava. Nous verrons comment ils nous aident à écrire du code concis et de qualité."
+                        }
+                    ],
+                    tags: ["Java", "Guava", "Lombok"]
                 }
             ]
         },
