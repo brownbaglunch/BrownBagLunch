604cc2e0ce8200044302a34dab8f1fa93d5944e1
Nathaniel Richand <nrichand@gmail.com>
2013-08-23 16:10:49 +0200

    adding a button to contact a bagger

--- js/baggersUtils.js
+++ js/baggersUtils.js
@@ -1,5 +1,6 @@
 var map;
-$(document).ready(function () {
+
+function createMapWithVilles() {
     map = new GMaps({
         div: '#map',
         lat: 47.081012,
@@ -10,21 +11,35 @@ $(document).ready(function () {
 
     for (var villeIndex = 0; villeIndex < data['villes'].length; villeIndex++) {
         var ville = data['villes'][villeIndex];
-        createVilleMarker(ville);
+        createAVilleMarkerInTheMap(ville);
     }
+}
 
-    function createVilleMarker(ville) {
-        var action = function (e) {
-            window.location = window.location.toString().split('#')[0] + "#" + ville['name'];
-        };
-        map.addMarker({
-            lat: ville['lat'],
-            lng: ville['lng'],
-            title: ville['name'],
-            click: action
-        });
-    }
-});
+function createAVilleMarkerInTheMap(ville) {
+    var action = function () {
+        window.location = window.location.toString().split('#')[0] + "#" + ville['name'];
+    };
+    map.addMarker({
+        lat: ville['lat'],
+        lng: ville['lng'],
+        title: ville['name'],
+        click: action
+    });
+}
+
+function startGoogleAnalytics() {
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-38399904-1']);
+    _gaq.push(['_trackPageview']);
+    (function () {
+        var ga = document.createElement('script');
+        ga.type = 'text/javascript';
+        ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(ga, s);
+    })();
+}
 
 function addPaireImpaire(){
      for (var villeIndex = 0; villeIndex < data['villes'].length; villeIndex++) {
