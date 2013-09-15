5b80d9d0b74a2122a5cf338a2b91a89cc6f03bb6
nrichand <nrichand@xebia.fr>
2013-09-15 22:52:28 +0200

    small cleaning and add opacity on logo

--- js/baggersUtils.js
+++ js/baggersUtils.js
@@ -27,20 +27,6 @@ function createAVilleMarkerInTheMap(ville) {
     });
 }
 
-function startGoogleAnalytics() {
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-38399904-1']);
-    _gaq.push(['_trackPageview']);
-    (function () {
-        var ga = document.createElement('script');
-        ga.type = 'text/javascript';
-        ga.async = true;
-        ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(ga, s);
-    })();
-}
-
 function shuffleAndAlternateBaggers(villesAndBaggers){
      _(villesAndBaggers.villes).map(function(ville){
          ville.baggers = _.shuffle(ville.baggers);
