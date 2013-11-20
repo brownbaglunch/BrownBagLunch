8500bb5ac81b4757c1264d3fbdf2c4ea52b3bd22
nrichand <nrichand@xebia.fr>
2013-11-20 22:55:10 +0100

    add rh part

---
+++ rh/js/baggers.js
@@ -0,0 +1,90 @@
+var data = {
+    villes: [
+        {
+            name: "Paris",
+            ville_img: "img/villes/BBL.jpg",
+            lat: 48.856614,
+            lng: 2.352222,
+
+            baggers: [
+                {
+                    name: "Camille Roux",
+                    bio: "Passionné d'entrepreneuriat et de Lean Startup • Coach en Business Model • Directeur associé de Human Coders, centre de formation pour développeur",
+                    picture: "http://www.gravatar.com/avatar/29668d9eaaf486727a446f7d03c59f07.jpg?s=250",
+                    websites: [
+                        {title: "Conseil en Business Model", href: "http://www.camilleroux.com/formation-lean-startup-accompagnement-conseil-business/"},
+                        {title: "Human Coders", href: "http://humancoders.com"},
+                    ],
+                    twitter: "CamilleRoux",
+                    contact: "contact@camilleroux.com",
+                    mail: "contact@camilleroux.com",
+                    location: "Paris",
+                    sessions: [
+                        {
+                            title: "Lean Startup",
+                            summary: "Découvrez cette approche pramagtique permettant de lancer des sociétés/produits/services en un temps record"
+                        }
+                    ],
+                    tags: ["Entrepreneuriat", "Lean Startup", "Business Model Canvas"]
+                },
+                {
+                    name: "Ameur Yannick",
+                    bio: "Coach, Formateur Agile, Facilitateur",
+                    picture: "http://m.c.lnkd.licdn.com/media/p/1/005/012/1d8/0d5df68.jpg",
+                    websites: [
+                        {title: "Site web", href: "http://www.agilenco.fr/"},
+                        {title: "Blog Agile N Co", href: "http://yannick.ameur.free.fr/"},
+                        {title: "LinkedIn", href: "http://fr.linkedin.com/in/yannickameur/"}
+                    ],
+                    twitter: "yannickAmeur",
+                    contact: "yannick.ameur@gmail.com",
+                    mail: "yannick.ameur@gmail.com",
+                    location: "Paris",
+                    sessions: [
+                        {
+                            title: "Rétrospective",
+                            summary: "Je vous propose d'animer et faciliter votre rétrospective sur un format d'une heure."
+                        },
+                        {
+                            title: "Serious Games",
+                            summary: "Disccuter d'un thème de votre choix sur l'agilité méthodologie ou technique. Attention brainstorming collectif animer par l'utilisation d'un sérious Game."
+                        },
+                        {
+                            title: "Coaching Personnel",
+                            summary: "Envis d'être coaché ? Vous vous posez des questions sur vos projets professionnels et personnel, offrez vous une scéance de Coaching format 45mn."
+                        }
+                    ],
+                    tags: ["Agile", "Scrum", "Scrumban","Coach","formation","formateur","lean","kanban","TDD", "Test"]
+                },
+                {
+                    name: "François Sarradin",
+                    bio: "Consultant Java et &#955; développeur",
+                    picture: "http://m.c.lnkd.licdn.com/media/p/7/000/240/3bd/3dea7b4.jpg",
+                    websites: [
+                        {title: "Blog personnel", href: "http://kerflyn.wordpress.com/"},
+                        {title: "Blog Xebia", href: "http://blog.xebia.fr/author/fsarradin"}
+                    ],
+                    twitter: "fsarradin",
+                    contact: "<a href='http://kerflyn.wordpress.com/2013/02/06/bbl/'>Brown Bag Lunch (BBL)</a>",
+                    mail: "fsarradin@gmail.com",
+                    location: "Paris, Ouest parisien (+ Nantes), remote",
+                    sessions: [
+                        {
+                            title: "A Web Framework in Scala from Scratch in One Hour",
+                            summary: "(live coding - en/fr) Discover Scala through the development of an efficient small tested Web framework."
+                        },
+                        {
+                            title: "Kata : un type Option en Java",
+                            summary: "(live coding) Tests unitaires et refactoring pour un type Option en Java"
+                        },
+                        {
+                            title: "Une alternative à AOP ?",
+                            summary: "(poney / live coding / présentation) Découvrez quelle alternative la programmation fonctionnelle propose face à AOP (session jouée à Devoxx FR 2013 sous le titre \"FlatMap Zat Shit\")"
+                        }
+                    ],
+                    tags: ["Java", "Scala", "Programmation fonctionnelle"]
+                }
+            ]
+        }
+    ]
+};
