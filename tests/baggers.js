7eb5f02ca3a6aaa38d56e75465f28853502b92fe
Janos Gyerik <janos@xw8400>
2013-12-06 13:46:59 +0100

    removed unnecessary condition for truth-y values

--- tests/baggers.js
+++ tests/baggers.js
@@ -1,12 +1,12 @@
 test('there are cities in data.villes', function() {
-  ok(data.villes.length > 0);
+  ok(data.villes.length);
 });
 
 test('there are baggers in each data.villes.baggers', function() {
   var cities = data.villes;
   for (var i in cities) {
     var city = cities[i];
-    ok(city.baggers.length > 0);
+    ok(city.baggers.length);
   }
 });
 
