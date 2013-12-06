ee718a7bc34ae84eb0a697b24d654516eb0495bc
Janos Gyerik <janos@xw8400>
2013-12-06 08:00:38 +0100

    verify that if there is a ref_ville, it is valid

--- tests/baggers.js
+++ tests/baggers.js
@@ -9,3 +9,20 @@ test('there are baggers in each data.villes.baggers', function() {
     ok(city.baggers.length > 0);
   }
 });
+
+test('if there is a ref_ville, it is valid', function() {
+  var bagger = walk_baggers_until(function(bagger) {
+    return bagger.ref_ville;
+  });
+  if (bagger) {
+    ok(function() {
+      var cities = data.villes;
+      for (var i in cities) {
+        var city = cities[i];
+        if (city.name == bagger.ref_ville) return 1;
+      }
+    }(), 'ref_ville ' + bagger.ref_ville + ' is valid');
+  } else {
+    ok('no baggers with ref_ville');
+  }
+});
