d88bf6c954099a2d0be480256c4ee6d189e48a4c
Janos Gyerik <janos@xw8400>
2013-12-06 13:51:36 +0100

    added/improved comments of assertions

--- tests/baggers.js
+++ tests/baggers.js
@@ -6,12 +6,12 @@ test('there are baggers in each data.villes.baggers', function() {
   var cities = data.villes;
   for (var i in cities) {
     var city = cities[i];
-    ok(city.baggers.length);
+    ok(city.baggers.length, 'there are baggers in ' + city.name);
   }
 });
 
 test('if there is a ref_ville, it is valid', function() {
-  var matches = filter_baggers(function(bagger) {
+  var matches = filter_baggers(data, function(bagger) {
     return bagger.ref_ville;
   });
   if (matches.length) {
@@ -21,7 +21,7 @@ test('if there is a ref_ville, it is valid', function() {
       ok(get_city_by_name(data, name), 'ref_ville ' + name + ' is valid');
     }
   } else {
-    ok('no baggers with ref_ville');
+    ok(true, 'no baggers with ref_ville, so ok');
   }
 });
 
@@ -34,8 +34,6 @@ test('attributes correctly copied from ref_ville', function() {
     { name: "Paris", baggers: [ { name: "Jack", contact: "somewhere" } ] },
     { name: "Tokyo", baggers: [ { name: "Jack", ref_ville: "Paris" } ] }
   ] };
-  ok(get_city_by_name(data, "Paris"));
-  ok(get_city_by_name(data, "Tokyo"));
   var jack_in_tokyo = data.villes[1].baggers[0];
   ok(!jack_in_tokyo.contact, 'Jack has no .contact field in original data');
   denormalize_data(data);
