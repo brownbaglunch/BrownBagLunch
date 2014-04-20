c07d8beb4069d397700cf7d12c1d2ec9393f44d1
kstrategy <contact@kstrategy.fr>
2014-04-20 19:43:55 +0200

    Update baggers.js
    Ajout de Xavier Maurin (Consultant UX à Bordeaux)

--- js/baggers.js
+++ js/baggers.js
@@ -3455,6 +3455,50 @@ var data = {
             ]
         },
         {
+            "name": "Xavier Maurin",
+            "bio": "CEO de KStrategy, consultant UX (User eXperience), Co-Fondateur de UX Bordeaux",
+            "picture": "http://www.jperf.com/images/jpl.jpg",
+            "websites": [
+                {
+                    "title": "KStrategy.fr",
+                    "href": "http://www.kstrategy.fr"
+                },
+                {
+                    "title": "UX-Bordeaux.com",
+                    "href": "http://www.ux-bordeaux.com"
+                },
+                {
+                    "title": "UXChallenges.com",
+                    "href": "http://www.uxchallenges.com"
+                }
+            ],
+            "twitter": "k_strategy",
+            "contact": "contact@kstrategy.fr",
+            "mail": "x.maurin@kstrategy.fr",
+            "location": "Bordeaux",
+            "sessions": [
+                {
+                    "title": "UX et stratégies",
+                    "summary": "L'Expérience Utilisateur s'impose aujourd'hui comme une dimension clé des projets technologiques et d'innovation. J'interviens comme consultant auprès de grands groupes, ETI, PME et start-ups et les accompagne dans la mise en oeuvre d'une démarche UX adaptée à leur savoire-faire et leur culture. En m'appuyant sur cette expérience acquise, je pourrai introduire la thématique de l'UX, montrer les différentess méthodes et outils permettant d'intégrer efficaement l'UX au coeur de l'organisation et des process de conception."
+                },
+                {
+                    "title": "UX Design : un nouveau métier",
+                    "summary": "Plus que jamais, les attentes des clients sont focalisées sur leur expérience d’utilisateur. Les applications métier aujourd’hui ne peuvent plus se réduire à délivrer uniquement des fonctionnalités, aussi riches et complètes soient-elles. La forte adoption des nouveaux usages (interfaces tactiles, adoption croissante du cloud, etc.) oblige les éditeurs de logiciel à investir massivement dans la notion d’Expérience Utilisateur, sous peine de subir un abandon progressif des produits qui ont fait pourtant leur succès pendant de nombreuses années. Il devient alors indispensable d'intégrer l'UX au coeur de la conception produit et de positionner des UX Designers dans les équipes. Je mettrai en lumière la réalité de ce métier aujourd'hui, à la croisée de différentes disciplines (conception d'interface, ergonomie, design, marketing) et m'attacherai à répondre aux questions suivantes  : Comment positionner ce nouveau métier dans l'entreprise ?  Quels sont les profils disponibles ? Comment intéragit-il avec les autres fonctions ? Quelles sont les conditions de réussite de son intégration ? Quels outils et méthodes utilise-t-il ? etc."
+                }
+            ],
+            "tags": [
+                "UX",
+                "Expérience Utilisateur",
+                "Expérience Client", 
+                "Design",
+                "Conception",
+                "Ergonomie"
+            ],
+            "cities": [
+                "Bordeaux"
+            ]
+        },
+        {
             "name": "Jean-Pierre Lerbscher",
             "bio": "Foundateur JavaPerf Consulting, expert performance & JEE",
             "picture": "http://www.jperf.com/images/jpl.jpg",
