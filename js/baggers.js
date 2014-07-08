6439ac94f22dd8783b7f32640b531063f7a065b9
Eric Lefevre-Ardant <eric.lefevre.ext@mappy.com>
2014-07-08 14:28:39 +0200

    Ajout de la présentation Extreme Programming

--- js/baggers.js
+++ js/baggers.js
@@ -1532,6 +1532,10 @@ var data = {
                 {
                     "title": "Blog personnel",
                     "href": "http://ericlefevre.net/"
+                },
+                {
+                    "title": "Podcast",
+                    "href": "http://unetassedethepodcast.com/"
                 }
             ],
             "twitter": "elefevre",
@@ -1540,8 +1544,14 @@ var data = {
             "location": "Paris",
             "sessions": [
                 {
-                    "title": "Boucles Etranges : Etranges Boucles",
+                    "title": "Boucles Etranges : Etranges Boucles - avec Guillaume Tardif",
                     "summary": "Une promenade décousue dans les limites du code source Java, à la manière de Douglas Hofstadter. Nous parlons de code, d'apparences trompeuses, d'art et de la vie."
+                },
+                {
+                    "title": "Extreme Programming - avec Cyrille Martraire",
+                    "summary": "Bien sûr, notre métier est sérieux ; le Software Craftmanship n'est-il pas avant tout le professionnalisme ? Bien sûr, le progrès vient d'articles scientifiques ardus et de nouveaux logiciels complexes.
+                                Mais vous ne rêvez pas parfois de repousser le cadre professionnel habituel ? D'aller plus loin et d'explorer ? De vous amuser avec du code jusqu'à ce que ça devienne n'importe quoi ?
+                                Dans cette session nous vous présenterons des expériences ésotériques, des jeux bizarres et des joyeux délires qui font réfléchir. Réfléchir ? D'habitude on n'a jamais le temps !"
                 }
             ],
             "tags": [
@@ -1568,7 +1578,7 @@ var data = {
             "location": "Paris",
             "sessions": [
                 {
-                    "title": "Boucles Etranges : Etranges Boucles",
+                    "title": "Boucles Etranges : Etranges Boucles - avec Eric Lefevre-Ardant",
                     "summary": "Une promenade décousue dans les limites du code source Java, à la manière de Douglas Hofstadter. Nous parlons de code, d'apparences trompeuses, d'art et de la vie."
                 }
             ],
@@ -1617,6 +1627,12 @@ var data = {
                 {
                     "title": "Votre sujet sur-mesure",
                     "summary": "Atelier sur papier, en code autour d'un kata original, présentation ou retour d'expérience sur l'un des thèmes qui me passionnent: TDD, BDD, DDD, Legacy Code, Style FP"
+                },
+                {
+                    "title": "Extreme Programming - avec Eric Lefevre-Ardant",
+                    "summary": "Bien sûr, notre métier est sérieux ; le Software Craftmanship n'est-il pas avant tout le professionnalisme ? Bien sûr, le progrès vient d'articles scientifiques ardus et de nouveaux logiciels complexes.
+                                Mais vous ne rêvez pas parfois de repousser le cadre professionnel habituel ? D'aller plus loin et d'explorer ? De vous amuser avec du code jusqu'à ce que ça devienne n'importe quoi ?
+                                Dans cette session nous vous présenterons des expériences ésotériques, des jeux bizarres et des joyeux délires qui font réfléchir. Réfléchir ? D'habitude on n'a jamais le temps !"
                 }
             ],
             "tags": [
