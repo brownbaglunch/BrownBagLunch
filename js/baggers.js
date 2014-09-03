54a75ea342da394d7489f076c02c2a921843770e
maxbonnet <maxime@bonnet.tel>
2014-09-03 16:02:10 +0200

    Update baggers.js
    Ajout d'une session pour Maxime Bonnet

--- js/baggers.js
+++ js/baggers.js
@@ -4451,7 +4451,11 @@ var data = {
                 {
                     "title": "Comprendre et Utiliser le MBTI",
                     "summary": "Le Myers Briggs Type Indicator (MBTI) est un outils dans la palette des coachs, managers ou toutes personnes souhaitant comprendre ses relations aux autres. Simple et facilement utiliseable au quotidien, le MBTI permet de mieux comprendre et dialoguer avec ses interlocuteurs"
-                }
+                },
+                {
+		    "title": "Utiliser Lego Serious Play pour animer votre Brainstorming",
+		    "summary": "Quand l'un des plus grands fabriquants de jouets décide d'utiliser sa production pour faciliter ses réunions, cela donne Lego Serious Play (http://www.seriousplay.com). Le jeu pour stimuler la créativité, ce n'est pas une idée nouvelle, mais LSP propose une dynamique très particulière basé sur le toucher et la manipulation. Les résultats sont étonnant et l'outil très puissant. Pour cette session je vous propose une mise en pratique rapide d'un atelier type de Lego Serious Play. (cette session est limité à 6 personnes)"
+		}
 
             ],
             "tags": [
