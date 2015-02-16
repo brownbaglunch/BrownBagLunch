078a20357aaee6e9aae498af82786150b0f36c86
Leonardo Noleto <noleto.leonardo@gmail.com>
2015-02-16 17:21:38 +0100

    Nouveau BBL sur Toulouse: anomaly detection.

--- js/baggers.js
+++ js/baggers.js
@@ -5710,20 +5710,24 @@ var data = {
         },
         {
             "name": "Leonardo Noleto",
-            "bio": "Data passionné et business geek. J'utilise l'informatique et les données pour rendre la vie des utilisateurs plus facile et confortable (et la mienne d'abord, il faut bien tester avant non ?). Mes passions: Java/Scala, Python/R, Vélo en libre service, les TEDx et les startups.",
+            "bio": "Data Scientist Freelance et business geek. J'utilise l'informatique et les données pour rendre la vie des utilisateurs plus facile et confortable (et la mienne d'abord, il faut bien tester avant non ?). Mes passions: Machine Learning, Java/Python/R, Vélo en libre service, les TEDx et les startups.",
             "picture": "img/baggers/leonardo-noleto-avatar.jpg",
             "websites": [
                 {"title": "LinkedIn", "href": "http://fr.linkedin.com/pub/leonardo-noleto/63/70b/820"}
             ],
             "twitter": "leonardo_noleto",
             "mail": "noleto.leonardo [at] gmail [dot] com",
-            "location": "Toulouse",
+            "location": "Toulouse et Agglomération. Ailleurs selon déplacements",
             "sessions": [{
                     "title": "Transfomez vos logs en insights avec ELK",
                     "summary": "Les logs sont une mine d’informations. Cependant, les transformer en métriques faciles à comprendre peut s'avérer une tâche ardue : chaque application a une structure de log différente, elles sont souvent décentralisées, les formats de timestamp sont divers et variés et les analyser nécessite souvent une expertise technique.  Avec la stack ELK -  ElasticSearch, Logstash et Kibana,  je vous montrerai que faire parler vos logs n'est plus qu'un jeu d'enfant !"
+                },
+                {
+                    "title" : "Anomaly Detection (a.k.a outlier detection)",
+                    "summary" : "Vous aimez jouer au détective ? Résoudre des mystérieuses affaires ? Cette présentation est faite pour vous : repérez les données inhabituelles, attrapez les fraudes, découvrez les intrus ! Je vous ferai un tour d’horizon des concepts et techniques pour détecter des anomalies dans un jeux de données. Des méthodes statistiques au machine learning nous allons voir quelques algorithmes et uses cases. Alors, élémentaire, mon cher Watson ?"
                 }
             ],
-            "tags": ["Kibana", "Big Data", "Valorisation données", "Métriques Business", "NoSQL"],
+            "tags": ["Machine learning", "Data Mining", "Kibana", "Big Data", "Valorisation données", "Métriques Business", "NoSQL"],
             "cities": ["Toulouse"]
         },
         {
